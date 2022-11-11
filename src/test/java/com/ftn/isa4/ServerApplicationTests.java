4c4ea8c8dd1eb8be8a3922ba1b2b8197413f616f
birdpersson <aleksandarvujasin@gmail.com>
2022-11-11 14:32:16 +0100

    Initial commit

---
+++ src/test/java/com/ftn/isa4/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.isa4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
