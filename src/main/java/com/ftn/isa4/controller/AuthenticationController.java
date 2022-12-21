2e58f13dab3c5d22d12bb17e05210236e1a76ff9
birdpersson <aleksandarvujasin@gmail.com>
2022-12-21 15:46:14 +0100

    Add email signup

--- src/main/java/com/ftn/isa4/controller/AuthenticationController.java
+++ src/main/java/com/ftn/isa4/controller/AuthenticationController.java
@@ -5,21 +5,21 @@ import com.ftn.isa4.dto.UserRequest;
 import com.ftn.isa4.dto.UserTokenState;
 import com.ftn.isa4.model.User;
 import com.ftn.isa4.security.TokenUtils;
+import com.ftn.isa4.service.EmailService;
 import com.ftn.isa4.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 
 @RestController
 @RequestMapping(value = "/auth", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -33,6 +33,9 @@ public class AuthenticationController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    EmailService emailService;
 
     @PostMapping("/login")
     public ResponseEntity<UserTokenState> createAuthenticationToken(
@@ -53,7 +56,27 @@ public class AuthenticationController {
 
     @PostMapping("/signup")
     public ResponseEntity<User> create(@RequestBody UserRequest userRequest) throws RuntimeException {
-        return new ResponseEntity<>(userService.save(userRequest), HttpStatus.CREATED);
+        User exists = userService.findByUsername(userRequest.getUsername());
+        if (exists != null) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        User user = userService.save(userRequest);
+        try {
+            emailService.sendRegistrationMail(user);
+        } catch (MailException e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/verify")
+    public ResponseEntity<User> verify(@RequestParam("token") String token) {
+        User user = userService.findByToken(token);
+        if (user == null)
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(userService.enable(user), HttpStatus.CREATED);
     }
 
 }
