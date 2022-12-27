8d38b4db33664bdaf29896938076ef838c0032ef
birdpersson <aleksandarvujasin@gmail.com>
2022-12-27 23:11:07 +0100

    Add user profile

--- src/main/java/com/ftn/isa4/controller/UserController.java
+++ src/main/java/com/ftn/isa4/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,12 @@ public class UserController {
         User user = userService.findByUsername(tokenUtils.getUsernameFromToken(tokenUtils.getToken(request)));
         user.setQuestionnaire(selected);
         return new ResponseEntity<>(userService.edit(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<User> view(HttpServletRequest request) {
+        User user = userService.findByUsername(tokenUtils.getUsernameFromToken(tokenUtils.getToken(request)));
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
 }
